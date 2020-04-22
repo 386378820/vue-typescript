2ccfdc064ec4176df849b72bb900c25dea7d0132
Tao LIU <liutao@bluepulse.cn>
2020-04-22 17:29:30 +0800

    init

---
+++ src/views/index.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  index
+</div>
+</template>
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+  @Component
+  export default class Test extends Vue {
+    private name: string;
+  }
+</script>
+
+<style scoped>
+
+</style>
